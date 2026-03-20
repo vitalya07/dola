24216562513454abdcc6a96ca707a395a7c592b8
Виталий <saifutdinov_vitalii@mail.ru>
2026-03-20 15:59:20 +0500

    gffdfg

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,20 +24,40 @@ setInterval(toggleImages, 3000);
     const header = document.querySelector('.header');
     const headerTopbar = document.querySelector('.header__top-bar');
     function hideHeader() {
-            if(window.scrollY >= 200) {
-                headerTopbar.classList.add('hide-top-header');
-                } else {
-                    headerTopbar.classList.remove('hide-top-header');
-                }
-            } if(window.scrollY >= 202) {
-                headerTopbar.style.display = 'none'
-                // headerTopbar.classList.add('hide')
-            } else {
-                headerTopbar.classList.remove('hide')
-            }
-            window.addEventListener('scroll', ()=> {
-                hideHeader();
-            }
-        );
-        
+      if(window.scrollY >= 200) {
+          headerTopbar.classList.add('hide-top-header');
+          } else {
+              headerTopbar.classList.remove('hide-top-header');
+          }
+      } if(window.scrollY >= 202) {
+          headerTopbar.style.display = 'none'
+          // headerTopbar.classList.add('hide')
+      } else {
+          headerTopbar.classList.remove('hide')
+      }
+      window.addEventListener('scroll', ()=> {
+          hideHeader();
+      }
+  );
+  //Открытие и закрытие вопроса
+  const faqsItem = document.querySelectorAll('.faqs__item');
+  const faqsBoxHide = document.querySelector('.faqs__box-hide');
+  const faqsBtn = document.querySelector('.faqs__btn');
+  const faqsBtnText = document.querySelector('.faqs__btn-text');
+  const faqsBtnImg = document.querySelector('.faqs__btn-img');
+  faqsItem.forEach(item => {
+    item.addEventListener('click', ()=> {
+      item.classList.toggle('open-answer')
+    })
+  });
+  faqsBtn.addEventListener('click', ()=> {
+    faqsBoxHide.classList.toggle('hide');
+    if(faqsBoxHide.classList.contains('hide')) {
+      faqsBtnText.innerHTML = `Show More`
+      faqsBtnImg.style.transform = `rotate(90deg)`
+    } else {
+      faqsBtnText.innerHTML = `Show less`
+      faqsBtnImg.style.transform = `rotate(270deg)`
+    }
+  })      
 });
